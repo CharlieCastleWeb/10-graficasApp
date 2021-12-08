1b01e2c9151dec99af740c4496b1da75cb8ff8a1
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2021-12-09 00:13:30 +0100

    Initial Commit Proyecto 10-graficasApp

---
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { BaseChartDirective  } from 'ng2-charts';
+
+@Component({
+  selector: 'app-grafica-barra',
+  templateUrl: './grafica-barra.component.html',
+  styles: [
+  ]
+})
+export class GraficaBarraComponent implements OnInit {
+
+  @Input() horizontal: boolean = false;
+
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true
+  };
+  public barChartType: ChartType = 'bar';
+
+
+  public barChartData: ChartData<'bar'> = {
+    labels: [ '2020', '2021', '2022', '2023', '2024', '2025', '2026' ],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#ED5F76', hoverBackgroundColor: 'red' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor: '#F763C4', hoverBackgroundColor: 'red' },
+      { data: [ 18, 38, 70, 59, 66, 80, 100 ], label: 'Series B', backgroundColor: '#D665E0', hoverBackgroundColor: 'red' }
+    ]
+  };
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
